9a6e9be8d404654f3c36db8c70924ed95cd6c35e
Yunni Lu <s0425513@stud.uni-frankfurt.de>
2022-01-30 16:28:16 +0100

    allow fetching multiple connections on init

--- backend/src/main/java/org/texttechnologylab/project/Gruppe_8_mittwoch_3/database/MongoDBConfig.java
+++ backend/src/main/java/org/texttechnologylab/project/Gruppe_8_mittwoch_3/database/MongoDBConfig.java
@@ -1,12 +1,14 @@
 package org.texttechnologylab.project.Gruppe_8_mittwoch_3.database;
 
+import java.util.List;
+
 public class MongoDBConfig{
     private String remoteHost;
     private String remoteDatabase;
     private String remoteUser;
     private String remotePassword;
     private int remotePort;
-    private String remoteCollection;
+    private List<String> remoteCollections;
 
 //    public Properties(String configJsonPath){
 //        ObjectMapper objectMapper = new ObjectMapper();
@@ -18,5 +20,5 @@ public class MongoDBConfig{
     public String getRemoteUser(){ return this.remoteUser;}
     public String getRemotePassword(){ return this.remotePassword;}
     public int getRemotePort(){ return this.remotePort;}
-    public String getRemoteCollection(){ return this.remoteCollection;}
+    public List<String> getRemoteCollections(){ return this.remoteCollections;}
 }
--- backend/src/main/java/org/texttechnologylab/project/Gruppe_8_mittwoch_3/database/MongoDBConnectionHandler.java
+++ backend/src/main/java/org/texttechnologylab/project/Gruppe_8_mittwoch_3/database/MongoDBConnectionHandler.java
@@ -2,9 +2,7 @@ package org.texttechnologylab.project.Gruppe_8_mittwoch_3.database;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.*;
@@ -21,8 +19,7 @@ import org.bson.conversions.Bson;
 public class MongoDBConnectionHandler {
     private MongoDBConfig config;
     private MongoDatabase database;
-    private MongoCollection<Document> protocolCollection;
-    private MongoCollection<Document> redeCollection;
+    private Map<String, MongoCollection<Document>> collections = new HashMap<>();
 
     public MongoDBConnectionHandler(String configPath) throws IOException {
         // Properties config = this.readConfig("Daten/config.json");
@@ -31,38 +28,62 @@ public class MongoDBConnectionHandler {
             MongoCredential credential = MongoCredential.createCredential(this.config.getRemoteUser(), this.config.getRemoteDatabase(), this.config.getRemotePassword().toCharArray());
             MongoClient mongoClient = new MongoClient(new ServerAddress(this.config.getRemoteHost(), this.config.getRemotePort()),credential, MongoClientOptions.builder().build());
             this.database = mongoClient.getDatabase(this.config.getRemoteDatabase());
-            this.protocolCollection = this.database.getCollection(this.config.getRemoteCollection());
-            this.redeCollection = this.database.getCollection("rede");
+            for (String collectionName: this.config.getRemoteCollections()){
+                this.collections.put(collectionName, this.database.getCollection(collectionName));
+            }
             System.out.println("Connect to database successfully");
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
     }
 
-    public void writeDocument(Document document){
-        this.protocolCollection.insertOne(document);
+    public void writeDocument(String collectionName, Document document){
+        try{
+            MongoCollection<Document> collection = this.getCollection(collectionName);
+            collection.insertOne(document);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    public Document getDocument(Bson query){
+    public Document getDocument(String collectionName, Bson query){
         try{
-            return this.protocolCollection.find(query).first();
+            MongoCollection<Document> collection = this.getCollection(collectionName);
+            return collection.find(query).first();
+        } catch (NullPointerException e){
+            System.err.println("MongoDBConnectionHandler.getDocument: collection " + collectionName + " not found!");
+            return null;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    public void updataDocument(Bson query, Document document){
+    public void updataDocument(String collectionName, Bson query, Document document){
+
     }
 
-    public Boolean deleteDocument(Bson query){
+    public Boolean deleteDocument(String collectionName, Bson query){
         try {
-            DeleteResult result = this.protocolCollection.deleteOne(query);
+            MongoCollection<Document> collection = this.getCollection(collectionName);
+            DeleteResult result = collection.deleteOne(query);
             System.out.println("Deleted document count: " + result.getDeletedCount());
             return true;
         } catch (MongoException me) {
             System.err.println("Unable to delete due to an error: " + me);
             return false;
+        }
+    }
+
+    public MongoCollection<Document> getCollection(String collectionName) {
+        try {
+            return this.collections.get(collectionName);
+        } catch (NullPointerException e) {
+            System.err.println("MongoDBConnectionHandler.getCollection: collection " + collectionName + " not found!");
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
     }
 
